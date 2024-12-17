6983bcbd106847fb937c533357994efed8ed3ed9
koskei-kipkoech <patrickwayy@gmail.com>
2024-12-17 09:23:53 +0300

    updates tables

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
 
-    recipes = relationship('Recipe', back_populates='category')
+    recipes = relationship('Recipe', back_populates='category', cascade='all,delete-orphans')
 
     def __repr__(self):
         return f'Category( id={self.id},  name="{self.name}" )'
@@ -20,17 +20,17 @@ class Recipe(Base):
     category_id = Column(Integer, ForeignKey('categories.id'))
 
     category = relationship('Category', back_populates='recipes')
-    ingredients = relationship('Ingredient', back_populates='recipes')
+    ingredients = relationship('Ingredient', back_populates='recipe', cascade='all, delete-orphans')
 
     def __repr__(self):
-        return f'Recipe( id={self.id},  name="{self.name}",  description="{self.description}",  category_id={self.category_id} )'
+        return f'Recipe( id={self.id},  name="{self.name}",  description="{self.description}", category_id={self.category_id} )'
 
 class Ingredient(Base):
     __tablename__ = 'ingredients'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     quantity = Column(Float, nullable=False)
-    unit = Column(String(10), nullable=False)
+    unit = Column(String(20), nullable=False)
     recipe_id = Column(Integer, ForeignKey('recipes.id'))
 
     recipes = relationship('Recipe', back_populates='ingredients')
